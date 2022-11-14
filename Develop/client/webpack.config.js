945a0050d9df673bd58f1e23d71494515b776ca8
Yasmin <bustamiyasmin@gmail.com>
2022-11-14 09:04:34 -0600

    webpack complete

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const { isBooleanObject } = require('util/types');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,11 +19,54 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor!'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: '.src-sw.js'
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'TE',
+        description: 'Text editor that will help make things easier',
+        background_color: '#ee8294',
+        theme_color: '#ee8294',
+        start_url:'./',
+        publichPath:'./',
+
+        icons: [{
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+        ],
+
+      }),
       
     ],
 
     module: {
-      rules: [
+      rules: [{
+        test: /\.css$/i,
+        use: [ 'style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['babel/plugin-proposal-object-rest-spread',
+          '@babel/transform-runtime'],
+          },
+
+        },
+      },
         
       ],
     },
